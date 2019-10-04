deb3eb06ae85d38b1e6b425aea4a1391d9d85018
xremix <tonidhoffmann@gmail.com>
2019-10-04 10:23:33 +0200

    loading data from the mongo db

--- src/handlers/goodbye.js
+++ src/handlers/goodbye.js
@@ -1,33 +1,39 @@
 'use strict';
 require('dotenv').config()
+var mongoClient = require('./services/mongoService');
 
 module.exports.sayGoodbye = async function(context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
 
-  if (req.query.name || (req.body && req.body.name)) {
-     var promise1 = new Promise(function (resolve, reject) {
+  // if (!req.query.name && !(req.body && req.body.name)) {
+  //   context.res = {
+  //     status: 400,
+  //     body: 'Please pass a name on the query string or in the request body',
+  //   };
+  //   return;
+  // }
+  var p = new Promise(function (resolve, reject) {
+    context.log("starting mongo client");
+    mongoClient.connect(context, function(err, db){
+      context.log('Connected to the mongo db, lets gather some data');
 
-         setTimeout(function () {
-           req.query.name = 'async ' + req.query.name;
-             resolve('foo');
-         }, 10);
+      if (err) throw err;
+      var dbo = db.db("prototype-db");
+      var query = {  };
+      dbo.collection("customers").find(query).toArray(function(err, result) {
+        if (err) throw err;
+        // console.log(result);
+        db.close();
+        resolve(result);
+      });
+    });
+  });
 
-     });
+  let resolve = await p;
+  context.log(resolve);
+  context.res = {
+    status: 200,
+    body: resolve,
+  };
 
-     context.log(promise1);
-
-     // Use await instead of promise.then(...)
-
-     let resolve = await promise1;
-     context.log(resolve);
-    context.res = {
-      // status: 200, /* Defaults to 200 */
-      body: 'Goodbye ' + (req.query.name || req.body.name),
-    };
-  } else {
-    context.res = {
-      status: 400,
-      body: 'Please pass a name on the query string or in the request body',
-    };
-  }
 };
--- src/handlers/services/mongoService.js
+++ src/handlers/services/mongoService.js
@@ -1,9 +1,11 @@
 var mongoClient = require("mongodb").MongoClient;
 
 
-exports.connect = function(){
+exports.connect = function(context, callback){
+  context.log("Going to connect to mongo db " + process.env.MONGO_URL);
   mongoClient.connect(process.env.MONGO_URL, function (err, db) {
-    db.close();
+    context.log("connection successfull, running callback now " + (typeof callback));
+    callback(err, db);
+    context.log("callback finished");
   });
-
 }
