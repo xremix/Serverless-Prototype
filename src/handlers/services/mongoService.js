b99d9412155a095a257ce07416c2164caad9511e
xremix <tonidhoffmann@gmail.com>
2019-10-04 11:37:18 +0200

    move db to env

--- src/handlers/services/mongoService.js
+++ src/handlers/services/mongoService.js
@@ -5,7 +5,7 @@ exports.connect = function(context, callback){
   context.log("Going to connect to mongo db " + process.env.MONGO_URL);
   mongoClient.connect(process.env.MONGO_URL, function (err, db) {
     context.log("connection successfull, running callback now " + (typeof callback));
-    var dbo = db.db("prototype-db");
+    var dbo = db.db(process.env.MONGO_DB);
     callback(err, db, dbo);
     context.log("callback finished");
   });
