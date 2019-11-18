bd0e204520f811f60fddafae7ded705c5f917cfc
xremix <tonidhoffmann@gmail.com>
2019-11-18 17:16:53 +0100

    more logging

--- src/handlers/services/mongoService.js
+++ src/handlers/services/mongoService.js
@@ -4,7 +4,7 @@ var mongoClient = require('mongodb').MongoClient;
 exports.connect = function(context, callback) {
   context.log('Going to connect to mongo db ' + process.env.MONGO_URL);
   mongoClient.connect(process.env.MONGO_URL, function(err, db) {
-    context.log('connection successfull, running callback now ' + (typeof callback));
+    context.log('[connect] connection successfull, running callback now ' + (typeof callback));
     var dbo = db.db(process.env.MONGO_DB);
     callback(err, db, dbo);
     context.log('callback finished');
@@ -14,11 +14,15 @@ exports.connect = function(context, callback) {
 exports.getPromiseConnection = function (context, callback){
   context.log('starting promise');
   return new Promise(function (resolve, reject) {
-    context.log('Going to connect to mongo db ' + process.env.MONGO_URL);
+    context.log('[promise connect] Going to connect to mongo db ' + process.env.MONGO_URL);
     mongoClient.connect(process.env.MONGO_URL, function(err, db) {
-      context.log('connection successfull, running callback now ' + (typeof callback));
+      context.log('[promise connect] connection successfull, running callback now ' + (typeof callback));
+      context.log('err');
+      context.log(err);
+      context.log('db');
+      context.log(db);
       var dbo = db.db(process.env.MONGO_DB);
-      context.log('starting mongo client');
+      context.log('[promise connect] starting mongo client');
 
       if (err) {
         context.log('error openning the connection ' + err);
