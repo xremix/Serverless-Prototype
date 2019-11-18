db8cd8decf70142447bcc8b077f5716f855356cb
xremix <tonidhoffmann@gmail.com>
2019-11-18 21:16:14 +0100

    all customers function

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Refer to [Serverless docs](https://serverless.com/framework/docs/providers/azure/guide/intro/) for more information.
 
+## Run locally
+```
+serverless offline
+serverless invoke local --function hello --data '{"pathParameters":{"name":"123456"}}'
+serverless invoke local --function addCustomer --data '{"name":"123456", "id": 9}'
+```
 
 ## Known Issues
 
---
+++ src/handlers/allCustomers.js
@@ -0,0 +1,20 @@
+'use strict';
+require('dotenv').config()
+var customerRepo = require('./repositories/customerRepository');
+
+module.exports.handler = async function(context, req) {
+  context.log('Starting all customers');
+  try {
+    let resolve = await customerRepo.find(context);
+    context.res = {
+      status: 200,
+      body: resolve
+    };
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: error
+    };
+  }
+
+};
